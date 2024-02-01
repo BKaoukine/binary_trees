4844a34cf4f402425d0be69c271941371db39c2f
BKaoukine <b.koukin@gmail.com>
2024-02-01 12:07:29 +0100

    6. Pre-order traversal

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -8,15 +8,15 @@
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
 
-	if(tree && func)
-		{
-			func(tree->n);  /* Process the current node */
+	if (tree && func)
+	{
+		func(tree->n);  /* Process the current node */
 
-			/* Recursively traverse the left subtree */
-			binary_tree_preorder(tree->left, func);
+		/* Recursively traverse the left subtree */
+		binary_tree_preorder(tree->left, func);
 
-			/* Recursively traverse the right subtree */
-			binary_tree_preorder(tree->right, func);
+		/* Recursively traverse the right subtree */
+		binary_tree_preorder(tree->right, func);
 
-		}
+	}
 }
